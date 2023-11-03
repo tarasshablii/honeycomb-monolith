2a44c11d541be745312da0d9d6367eca1eee7941
Taras Shablii <taras.shablii@gmail.com>
2023-11-03 12:16:41 +0200

    initial project structure setup

---
+++ microservices/media/src/test/java/dev/tarasshablii/media/ApplicationTest.java
@@ -0,0 +1,13 @@
+package dev.tarasshablii.media;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+	@Test
+	void contextLoads() {
+
+	}
+}
---
+++ microservices/sponsors/src/test/java/dev/tarasshablii/sponsors/ApplicationTest.java
@@ -0,0 +1,13 @@
+package dev.tarasshablii.sponsors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+	@Test
+	void contextLoads() {
+
+	}
+}
